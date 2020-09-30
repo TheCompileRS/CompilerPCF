49c44074943c1e6b9dc02446278b725b70399a5e
Roman Castellarin <romancastellarin@gmail.com>
2020-09-30 18:27:46 -0300

    [WIP] Started writing unittests
    Co-authored-by: Sebastián Zimmermann <zimmermannsebas@gmail.com>

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Parse" $ do
+    it "checks that sum is commutative" $ do
+      property $ \x y -> x + y == (y + x :: Int)
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+    it "throws an exception if used with an empty list" $ do
+      evaluate (head []) `shouldThrow` anyException
+
