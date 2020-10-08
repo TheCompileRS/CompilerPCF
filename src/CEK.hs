8e50f56ac27f6dcf4472bb070d22299e4f22602f
Roman Castellarin <romancastellarin@gmail.com>
2020-10-08 17:05:11 -0300

    search and destroy now preserve types (but not info, by design) [fix] valToTerm now substitutes free vars using environment
    Co-authored-by: Sebastián Zimmermann <zimmermannsebas@gmail.com>

--- src/CEK.hs
+++ src/CEK.hs
@@ -13,11 +13,12 @@ module CEK ( search, destroy,valToTerm ) where
 import Lang
 import MonadPCF
 import Common
+import Subst (substN)
 --import Debug.Trace (trace)
 
 data Val = VNat Int
-         | CFun Env Name Term 
-         | CFix Env Name Name Term
+         | CFun Env Name Ty Term 
+         | CFix Env Name Ty Name Ty Term
      deriving Show
 
 type Env = [Val]
@@ -32,12 +33,12 @@ type Kont = [Fr]
 
 valToTerm :: Val -> Term
 valToTerm v = case v of
-    VNat n        -> Const NoPos (CNat n)
-    CFun _ x t    -> Lam NoPos x NatTy t
-    CFix _ f x t  -> Fix NoPos f NatTy x NatTy t
+    VNat n              -> Const NoPos (CNat n)
+    CFun e x xt t       -> substN (valToTerm <$> e) $ Lam NoPos x xt t
+    CFix e f ft x xt t  -> substN (valToTerm <$> e) $ Fix NoPos f ft x xt t
 
 search :: MonadPCF m => Term -> Env -> Kont -> m Val
---search t e k | trace ("search " ++ show t ++ " " ++ show k) False = undefined
+--search t e k | trace ("search " ++ show t ++ " <> " ++ show e ++ " <> " ++ show k) False = undefined
 search t e k = case t of
         UnaryOp _ op t'  -> search t' e (FUnaryOp op : k)
         IfZ _ t1 t2 t3   -> search t1 e (FIfz e t2 t3 : k)
@@ -49,24 +50,24 @@ search t e k = case t of
         -- TODO: does the type system guarantee that this won't go out of bounds?
         V _ (Bound n)    -> destroy (e !! n) k
         Const _ (CNat n) -> destroy (VNat n) k
-        Lam _ x _ t      -> destroy (CFun e x t) k
-        Fix _ f _ x _ t  -> destroy (CFix e f x t) k
+        Lam _ x xt t      -> destroy (CFun e x xt t) k
+        Fix _ f ft x xt t  -> destroy (CFix e f ft x xt t) k
 
 destroy :: MonadPCF m => Val -> Kont -> m Val
---destroy v k | trace ("destroy " ++ show v ++ " " ++ show k) False = undefined
+--destroy v k | trace ("destroy " ++ show v ++ " <> " ++ show k) False = undefined
 destroy v k = case k of
         []   -> return v
         fr:ks -> case fr of
-            FUnaryOp Pred             -> let VNat n = v
-                                         in if n == 0 
-                                             then destroy (VNat 0) ks
-                                             else destroy (VNat $ n-1) ks
-            FUnaryOp Succ             -> let VNat n = v in destroy (VNat $ n+1) ks
-            FIfz e t1 t2              -> let VNat n = v
-                                         in if n == 0
-                                             then search t1 e ks
-                                             else search t2 e ks
-            FApp e t                  -> search t e (FClosure v : ks)
-            FClosure (CFun e _ t)     -> search t (v : e) ks
-            FClosure c@(CFix e _ _ t) -> search t (v : c : e) ks
-            caca -> error $ show caca
+            FUnaryOp Pred                 -> let VNat n = v
+                                             in if n == 0 
+                                                 then destroy (VNat 0) ks
+                                                 else destroy (VNat $ n-1) ks
+            FUnaryOp Succ                 -> let VNat n = v in destroy (VNat $ n+1) ks
+            FIfz e t1 t2                  -> let VNat n = v
+                                             in if n == 0
+                                                 then search t1 e ks
+                                                 else search t2 e ks
+            FApp e t                      -> search t e (FClosure v : ks)
+            FClosure (CFun e _ _ t)       -> search t (v : e) ks
+            FClosure c@(CFix e _ _ _ _ t) -> search t (v : c : e) ks
+
